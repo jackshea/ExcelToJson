b5a777ecefdd52753557971a0385690a09863d23
谢在奎 <jackshea@163.com>
2021-02-03 18:31:51 +0800

    建立项目结构

---
+++ ExcelToJsonConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExcelToJsonConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ExcelToJsonLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExcelToJsonLib
+{
+    public class Class1
+    {
+    }
+}
