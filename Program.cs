992f58b16f05c819bb875617b3f919fa8044c557
谢在奎 <jackshea@163.com>
2021-02-03 18:25:55 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExcelToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
